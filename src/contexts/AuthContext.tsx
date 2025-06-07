fa91e5ad7afeff7492cda08ec5495a358aac413d
nadsonsousa95 <nadsonbcs95@gmail.com>
2025-06-07 12:26:47 -0300

    feat: criando context api

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,64 @@
+import { useEffect, useState, createContext} from 'react';
+import type { ReactNode } from 'react';
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from '../services/firebaseConnection';
+
+interface IAuthProviderProps{
+    children: ReactNode
+}
+
+interface IuserProps{
+    uid: string,
+    name: string | null,
+    email: string | null
+}
+
+
+type AuthContextData = {
+    signed: boolean,
+    loadingAuth: boolean
+}
+
+export const AuthContext = createContext({} as AuthContextData)
+
+function AuthProvider({children}: IAuthProviderProps){
+    const [user, setUser] = useState<IuserProps | null>(null); //signed: false
+    const [loadingAuth, setLoadingAuth] = useState(true)
+
+    useEffect(()=>{
+        const unsub = onAuthStateChanged(auth, (user)=>{
+            if(user){
+                //tem usuario logado
+                setUser({
+                    uid: user.uid,
+                    name: user?.displayName,
+                    email : user?.email
+                })
+                setLoadingAuth(false)
+            }
+            else{
+                //nao tem usuario logado
+                setUser(null)
+                setLoadingAuth(false)
+            }
+        })
+        return()=>{
+            unsub();
+        }
+
+    }, [])
+
+
+    return(
+        <AuthContext.Provider 
+        value={{ 
+            signed : !!user,
+            loadingAuth
+        }}
+        >
+           {children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthProvider;
